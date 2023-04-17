744f72be6a311d3ab2798588d35d6881b787ad67
Juhász Csaba József <h870697@stud.u-szeged.hu>
2023-04-17 21:28:11 +0200

    #4 Add admin auth middleware

---
+++ app/Http/Middleware/AuthenticateAdmin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticateAdmin
+{
+    /**
+     * Check if the logged in user has admin rights.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!Auth::user()->is_admin) {
+            return redirect()->to('dashboard');
+        }
+        return $next($request);
+    }
+}
