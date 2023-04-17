54edb300519a109217c8b2e775fc0cc7805bcff5
Juhász Csaba József <h870697@stud.u-szeged.hu>
2023-04-17 11:53:08 +0200

    #1 Create migrations for users, pizzas and orders

--- database/migrations/2023_04_17_094223_create_pizzas_table.php
+++ database/migrations/2023_04_17_094223_create_pizzas_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pizzas', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('category');
+            $table->string('img');
+            $table->integer('price');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pizzas');
     }
 };
